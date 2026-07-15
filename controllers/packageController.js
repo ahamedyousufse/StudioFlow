247530e948c7d42342b275a12f4d808292fadd84
Ahamed Yousuf <ahamedyousuf5859@gmail.com>
2026-07-15 18:04:14 +0530

    minor fixes

--- controllers/packageController.js
+++ controllers/packageController.js
@@ -4,7 +4,7 @@ const packageController = {
   getAllPackages: (req, res) => {
     packageModel.findAll((err, rows) => {
       if (err) {
-        return console.error("something went wrong: ", err.message);
+        return res.status(500).json({ error: "Internal Server Error" });
       } else {
         res.status(200).json({ data: rows });
       }
@@ -19,14 +19,19 @@ const packageController = {
         return res.status(500).json({ error: "Internal Server Error" });
       }
       if (!row) {
-        return res.status(400).json({ error: "package not found" });
+        return res.status(404).json({ error: "package not found" });
       }
       res.status(200).json({ package: row });
     });
   },
 
   createPackage: (req, res) => {
-    const packageData = [req.body.name, req.body.price, req.body.duration, req.body.description];
+    const packageData = [
+      req.body.name,
+      req.body.price,
+      req.body.duration,
+      req.body.description,
+    ];
 
     // query to insert new packages to the database
     if (!packageData[0]) {
@@ -37,21 +42,24 @@ const packageController = {
       return res.status(400).json({ error: "duration con not be empty!" });
     }
 
-    packageModel.create(
-      packageData,
-      (err) => {
-        if (err) {
-          console.error("error creating package: ", err.message);
-          return res.status(500).json({ error: "Internal Server Error" });
-        } else {
-          res.status(200).json({ message: "package created successfully" });
-        }
-      },
-    );
+    packageModel.create(packageData, (err) => {
+      if (err) {
+        console.error("error creating package: ", err.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+      } else {
+        res.status(201).json({ message: "package created successfully" });
+      }
+    });
   },
 
   updatePackage: (req, res) => {
-    const packageData = [req.body.name, req.body.price, req.body.duration, req.body.description, req.params.id];
+    const packageData = [
+      req.body.name,
+      req.body.price,
+      req.body.duration,
+      req.body.description,
+      req.params.id,
+    ];
 
     packageModel.update(packageData, (err) => {
       if (err) {
@@ -64,18 +72,17 @@ const packageController = {
   },
 
   deletePackage: (req, res) => {
-  const packageId = req.params.id; // id of the package that is going to be deleted
+    const packageId = req.params.id; // id of the package that is going to be deleted
 
-  packageModel.delete(packageId, (err) => {
-    if(err){
-      console.error("error deleting the package: ", err.message);
-      return res.status(500).json({error: err.message});
-    } else {
-      res.status(200).json({message: "deleted package successfully"});
-    }
-  });
-  
-}
+    packageModel.delete(packageId, (err) => {
+      if (err) {
+        console.error("error deleting the package: ", err.message);
+        return res.status(500).json({ error: err.message });
+      } else {
+        res.status(200).json({ message: "deleted package successfully" });
+      }
+    });
+  },
 };
 
 module.exports = packageController;
