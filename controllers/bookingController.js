beb0e25f280e6282aeb158a43a53ccc801b97f27
Ahamed Yousuf <ahamedyousuf5859@gmail.com>
2026-07-17 07:35:51 +0530

    add update booking feature

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -20,7 +20,9 @@ const bookingController = {
       if (err) {
         console.error(`error getting booking with id of ${id}: ${err.message}`);
         return;
-      } else if (!row) {
+      }
+
+      if (!row) {
         return res.status(404).json({ error: "Booking not found" });
       }
 
@@ -49,7 +51,13 @@ const bookingController = {
       package_id,
     ];
 
-    const error = validateInput(bookingData);
+    const error = validateInput(
+      customer_name,
+      phone,
+      event_date,
+      venue,
+      package_id,
+    );
 
     if (error) {
       return res.status(400).json({ error });
@@ -57,9 +65,11 @@ const bookingController = {
 
     packageModel.findById(package_id, (err, row) => {
       if (err) {
-        console.error(`error finding booking: ${err.message}`);
+        console.error(`error finding package: ${err.message}`);
         return res.status(500).json({ error: "internal server error" });
-      } else if (!row) {
+      }
+
+      if (!row) {
         return res.status(404).json({ error: "package not found!" });
       }
 
@@ -74,30 +84,102 @@ const bookingController = {
     });
   },
 
-  updateBooking: (req, res) => {},
+  updateBooking: (req, res) => {
+    const booking_id = req.params.id;
+    const {
+      customer_name,
+      phone,
+      email,
+      event_date,
+      venue,
+      notes,
+      package_id,
+    } = req.body;
+
+    const bookingData = [
+      customer_name,
+      phone,
+      email,
+      event_date,
+      venue,
+      notes,
+      package_id,
+      booking_id
+    ];
+
+    bookingModel.findById(booking_id, (err, row) => {
+      if (err) {
+        console.error(`error finding booking for update: ${err.message}`);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+
+      if (!row) {
+        return res.status(404).json({ error: "booking doesn't exist" });
+      }
+    });
+
+    packageModel.findById(package_id, (err, row) => {
+      if (err) {
+        console.error(
+          `error finding package for booking updates: ${err.message}`,
+        );
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+
+      if (!row) {
+        return res.status(404).json({ error: "package doesn't exist" });
+      }
+    });
+
+    const error = validateInput(
+      customer_name,
+      phone,
+      event_date,
+      venue,
+      package_id,
+    );
+
+    if (error) {
+      return res.status(400).json({ error });
+    }
+
+    bookingModel.update(bookingData, function (err) {
+      if (err) {
+        console.error(`errro updating booking: ${err.message}`);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+
+      if (this.changes === 0) {
+        return res.status(500).json({ error: "couldn't udpate booking" });
+      }
+
+      res.status(201).json({ message: "booking updated successfully" });
+    });
+  },
 
   deleteBooking: (req, res) => {},
 };
 
-function validateInput(bookingData) {
+function validateInput(customer_name, phone, event_date, venue, package_id) {
   // validating customer name, phone, event date, venue, package id
-  if (!bookingData[0]) {
+  // phone validation should validate more parameters (eg. min lenght, max length etc..)
+  if (!customer_name) {
     return "name cannot be empty";
   }
-  if (!bookingData[1]) {
+  if (!phone) {
     return "phone number cannot be empty";
   }
-  if (isNaN(bookingData[1])) {
+  if (isNaN(phone)) {
     return "enter phone number in numbers";
   }
-  if (!bookingData[3]) {
+  if (!event_date) {
     return "event date cannot be empty";
   }
-  if (!bookingData[4]) {
+  if (!venue) {
     return "venue cannot be empty";
   }
-  if (!bookingData[6]) {
-    return "id cannot be empty";
+  if (!package_id) {
+    return "package id cannot be empty";
   }
 
   return null;
